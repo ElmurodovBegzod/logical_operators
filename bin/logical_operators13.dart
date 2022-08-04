67fc8ecd9b96a15f224b5966996018fd5c492111
Begzod <ebegzod477@gmail.com>
2022-08-04 21:56:14 +0500

    code13

--- bin/logical_operators13.dart
+++ bin/logical_operators13.dart
@@ -8,4 +8,14 @@ Given a two-digit integer a,  check the following statement "All digits sum is e
         bool: answer
 */
 
-void main() {}
+bool func(a) {
+  int x = (a % 10) + (a ~/ 10);
+  if (x % 2 != 1) {
+    return true;
+  }
+  return false;
+}
+
+void main() {
+  print(func(13));
+}
